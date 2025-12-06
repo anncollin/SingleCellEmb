fea96138dc2aff71d9aa2f0367dbd8e8f292c4a5
anncollin <anne-so.collin@hotmail.com>
2025-12-06 15:11:53 +0100

    .

--- SSL/eval_dino.py
+++ SSL/eval_dino.py
@@ -275,8 +275,7 @@ def evaluate_dino_experiment(cfg: Dict, use_callibration: bool):
     # ======================================================================
     # 3) EMBEDDING CACHE (ALWAYS FULL DATASET)
     # ======================================================================
-
-   hostname = socket.gethostname()
+    hostname = socket.gethostname()
     has_dataset = ("anncollin" in hostname) or ("orion" in hostname)
 
     all_drugs = load_drug_list(cfg["label_path"])
