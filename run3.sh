76d3f5effe703c709f70293431c0e5977e635823
anncollin <anne-so.collin@hotmail.com>
2026-03-05 14:09:42 +0100

    .

--- run3.sh
+++ run3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=td3
+#SBATCH --job-name=td2
+#SBATCH --partition=gpu
 #SBATCH --gres=gpu:GeForceRTX2080Ti:1
-#SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=32G
 #SBATCH --time=48:00:00
